0dc91f64058e758e69ca981a8262d0025171cb94
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-01-02 02:25:29 +0700

    script card animation slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,7 +11,7 @@ hamburger.addEventListener("click", function () {
 
 // Hide the floating menu when user clicks on anywhere
 
-window.addEventListener("click", function (e) {
+document.addEventListener("click", function (e) {
   if (e.target != hamburger && e.target != navbarmenu) {
     hamburger.classList.remove("hamburger-active");
     navbarmenu.classList.add("hidden");
@@ -45,15 +45,43 @@ function backToTop() {
 }
 
 // image show when on window view
-const targetElement = document.querySelectorAll("#image-show1, #image-show2, #image-show3, #image-show4, #hero-img, #contact-button, #contact-buttonfooter, #myproject-button,#title-side");
-for (const element of targetElement) {
+const targetImg = document.querySelectorAll("#hero-img, #contact-button, #contact-buttonfooter, #myproject-button");
+for (const elementImg of targetImg) {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        element.classList.add("visible-img");
-        observer.unobserve(element);
+        elementImg.classList.add("visible-img");
+        observer.unobserve(elementImg);
       }
     });
   });
-  observer.observe(element);
+  observer.observe(elementImg);
+}
+
+// card show when on window animation to the left side
+const targetCardLeft = document.querySelectorAll("#title-side,#card-1,#card-3");
+for (const elementCardLeft of targetCardLeft) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        elementCardLeft.classList.add("visible-card");
+        observer.unobserve(elementCardLeft);
+      }
+    });
+  });
+  observer.observe(elementCardLeft);
+}
+
+// card show when on window animation to the right side
+const targetCardRight = document.querySelectorAll("#card-2, #card-4");
+for (const elementCardRight of targetCardRight) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        elementCardRight.classList.add("visible-card");
+        observer.unobserve(elementCardRight);
+      }
+    });
+  });
+  observer.observe(elementCardRight);
 }
